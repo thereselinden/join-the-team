7f3e2eaa3b03e271110d7557eee2215bef72287e
thereselinden <theresemlinden@gmail.com>
2021-04-14 17:32:24 +0200

    created custom hook for user input

---
+++ src/hooks/useInput.js
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+//custom hook
+
+const useInput = initialValues => {
+  const [values, setValues] = useState(initialValues);
+  console.log(values);
+  return [
+    values, //returning the values
+    e => {
+      //returning an onchange function
+      setValues({
+        ...values, //keep all the values in there
+        [e.target.name]: e.target.value, //based on the name of the input field we will update its value
+      });
+    },
+  ];
+};
+export default useInput;
