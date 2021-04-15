320e545e09bbe3fe761b69d3697ca20edc5a9204
thereselinden <theresemlinden@gmail.com>
2021-04-15 16:37:15 +0200

    Refactored code

--- src/screen/SignUpScreen.js
+++ src/screen/SignUpScreen.js
@@ -4,47 +4,43 @@ import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 
 import TeamMembers from '../components/TeamMembers';
-import RegisterForm from '../components/RegisterForm';
+import Register from '../components/Register';
 
 import colors from '../theme/colors.json';
 import backgroundImage from '../assets/background.svg';
-import Typography from '@material-ui/core/Typography';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles(theme => ({
   root: {
     height: '100vh',
     display: 'flex',
-    justifyContent: 'center',
   },
   members: {
     backgroundImage: `url(${backgroundImage})`,
     background: colors.primaryBackground.color,
+    display: 'flex',
   },
-  title: {
-    color: colors.primary.color,
-    margin: 0,
-    textAlign: 'center',
+  register: {
+    display: 'flex',
+    //flexDirection: 'column',
+    //alignSelf: 'center',
+    justifyContent: 'center',
   },
-  register: {},
 }));
 
 const SignUpScreen = () => {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
+    <section className={classes.root}>
       <Grid container spacing={3}>
-        <Grid item xs={12} sm={4} className={classes.members}>
-          <Typography variant="h2" className={classes.title} xs={22}>
-            Join the Team
-          </Typography>
+        <Grid item xs={12} sm={3} className={classes.members}>
           <TeamMembers />
         </Grid>
-        <Grid item xs={12} sm={8} className={classes.register}>
-          <RegisterForm />
+        <Grid item xs={12} sm={9} className={classes.register}>
+          <Register />
         </Grid>
       </Grid>
-    </div>
+    </section>
   );
 };
 export default SignUpScreen;
